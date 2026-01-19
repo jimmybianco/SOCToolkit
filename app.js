278d5828078e84edf7b425c375745072c3ca14d8
Jimmy <26360492+jimmybianco@users.noreply.github.com>
2026-01-19 14:18:58 +0100

    CyberChef Data

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ const sources = {
         {name:"HudsonRock Infostealer (Username)", url:"https://cavalier.hudsonrock.com/api/json/v2/osint-tools/search-by-username?username={data}", usesDomain:false},
         {name:"ClickFix Hunter", url:"https://clickfix.carsonww.com/domains?query={data}"},
         {name:"WikiLeaks", url:"https://search.wikileaks.org/?query={data}", encode:false},
-        {name:"CyberChef", url:"https://gchq.github.io/CyberChef/", encode:false},
+        {name:"CyberChef", url:"https://gchq.github.io/CyberChef/#input={data}", encode:false},
         {name:"MXToolBox - EmailHeaders", url:"https://mxtoolbox.com/EmailHeaders.aspx", encode:false},
         {name:"No More Ransom", url:"https://www.nomoreransom.org/crypto-sheriff.php"},
     ]
@@ -410,6 +410,7 @@ document.querySelectorAll(".footer-btn").forEach(btn => {
     });
 });
 /* ================= END ================= */
+
 
 
 
